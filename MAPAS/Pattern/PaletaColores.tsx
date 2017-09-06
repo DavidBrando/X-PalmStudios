1559b29233e3a1f5671f348e5349b2a82eb7b0ba
hec17x <hec.medina.trabajo@gmail.com>
2017-09-06 20:02:47 +0200

    Patron suelo. Mapa 1.

---
+++ MAPAS/Pattern/PaletaColores.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="AmstradCPC_palette" tilewidth="1" tileheight="1" tilecount="27" columns="9">
+ <image source="AmstradCPC_palette.png" width="9" height="3"/>
+</tileset>
