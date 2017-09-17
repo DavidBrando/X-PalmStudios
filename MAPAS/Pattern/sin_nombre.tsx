9b883dd9c95470e911ceba4468fdde973a5f4b2e
DavidBrando <davidtbs@hotmail.es>
2017-09-17 21:13:17 +0200

    Mapa Ver: 1.0

---
+++ MAPAS/Pattern/sin_nombre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="edificios" tilewidth="4" tileheight="4" tilecount="4000" columns="100">
+ <image source="edificiios.png" width="400" height="160"/>
+</tileset>
