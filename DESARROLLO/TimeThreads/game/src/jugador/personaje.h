70d10f1a508ca0af63b4ce7245bc684d860225ef
hec17x <hec.medina.trabajo@gmail.com>
2017-09-28 20:35:12 +0200

    personaje

---
+++ DESARROLLO/TimeThreads/game/src/jugador/personaje.h
@@ -0,0 +1,42 @@
+// Data created with Img2CPC - (c) Retroworks - 2007-2015
+#ifndef _ASSETS_IMG_PERSONAJE_H_
+#define _ASSETS_IMG_PERSONAJE_H_
+
+#include <types.h>
+extern const u8 g_palette[16];
+
+#define G_PERSONAJE_00_W 16
+#define G_PERSONAJE_00_H 32
+extern const u8 g_personaje_00[16 * 32];
+#define G_PERSONAJE_01_W 16
+#define G_PERSONAJE_01_H 32
+extern const u8 g_personaje_01[16 * 32];
+#define G_PERSONAJE_02_W 16
+#define G_PERSONAJE_02_H 32
+extern const u8 g_personaje_02[16 * 32];
+#define G_PERSONAJE_03_W 16
+#define G_PERSONAJE_03_H 32
+extern const u8 g_personaje_03[16 * 32];
+#define G_PERSONAJE_04_W 16
+#define G_PERSONAJE_04_H 32
+extern const u8 g_personaje_04[16 * 32];
+#define G_PERSONAJE_05_W 16
+#define G_PERSONAJE_05_H 32
+extern const u8 g_personaje_05[16 * 32];
+#define G_PERSONAJE_06_W 16
+#define G_PERSONAJE_06_H 32
+extern const u8 g_personaje_06[16 * 32];
+#define G_PERSONAJE_07_W 16
+#define G_PERSONAJE_07_H 32
+extern const u8 g_personaje_07[16 * 32];
+#define G_PERSONAJE_08_W 16
+#define G_PERSONAJE_08_H 32
+extern const u8 g_personaje_08[16 * 32];
+#define G_PERSONAJE_09_W 16
+#define G_PERSONAJE_09_H 32
+extern const u8 g_personaje_09[16 * 32];
+#define G_PERSONAJE_10_W 16
+#define G_PERSONAJE_10_H 32
+extern const u8 g_personaje_10[16 * 32];
+
+#endif
